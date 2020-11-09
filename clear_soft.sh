8da8b6981b04f733cc390fba26ecdf229f98832e
Ruoxi Li <rli12314@login-3.deepthought2.umd.edu>
2020-11-08 19:40:38 -0500

    some exp results

---
+++ clear_soft.sh
@@ -0,0 +1,6 @@
+for i in {1..7}
+do
+  rm *${i}.sh
+done
+rm *.out
+
--- test_all.bash
+++ test_all.bash
@@ -4,10 +4,10 @@ do
   for j in $(seq 1 $i)
   do
     echo 0_${i}_0_${j}
-    sbatch -n 1 -N 1 --share -t 2:00:00 ./0_${i}_0_${j}.sh
+    sbatch -n 1 -N 1 --share -t 0:05:00 ./0_${i}_0_${j}.sh
     echo 0_${i}_1_${j}
-    sbatch -n 1 -N 1 --share -t 2:00:00 ./0_${i}_1_${j}.sh
+    sbatch -n 1 -N 1 --share -t 0:05:00 ./0_${i}_1_${j}.sh
     echo 1_${i}_1_${j}
-    sbatch -n 1 -N 1 --share -t 2:00:00 ./1_${i}_1_${j}.sh
+    sbatch -n 1 -N 1 --share -t 0:05:00 ./1_${i}_1_${j}.sh
   done
 done
