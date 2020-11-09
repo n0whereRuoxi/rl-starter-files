dba991e70e3f7a0844e5f831c99e0d9bb8087d79
Ruoxi <rli12314@terpmail.umd.edu>
2020-11-09 11:07:00 -0500

    dynamic plot

--- analyze_data.py
+++ analyze_data.py
@@ -2,6 +2,28 @@ from numpy import genfromtxt, isnan
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
+import math
+
+dist_feature_map = {}
+for d1 in [0,1]:
+    for d2 in [0,1]:
+        for i in range(1,8):
+            for j in range(1,8):
+                flag = False
+                feature = [d1,i,d2,j]
+                for file in os.listdir("/Users/ruoxili/Documents/NRL/rl-starter-files/storage"):
+                    if file.startswith('{}_{}_{}_{}'.format(feature[0], feature[1], feature[2], feature[3])):
+                        flag = True
+                if flag:
+                    x = [d1, i]
+                    y = [d2, j]
+                    d = math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
+                    dist_feature_map[d] = feature
+print(dist_feature_map.keys())
+
+def closest(lst, K): 
+    return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
 
 def get_all_data(feature):
     all_data = []
@@ -13,17 +35,39 @@ def get_all_data(feature):
             all_data.append(data)
     return all_data
 
-all_data = get_all_data([1,3,1,1])
-print(len(max(all_data, key=len)))
-rreturn_means = []
-num = 75
-for i in range(num):
-    rreturn_mean = []
-    for data in all_data:
-        if i < len(data):
-            rreturn_mean.append(data[i][1])
-    rreturn_means.append(rreturn_mean)
 fig1, ax1 = plt.subplots()
-ax1.set_title('Basic Plot')
-ax1.boxplot(rreturn_means)
+bar_dist = plt.axes([0.15, 0.05, 0.65, 0.03])
+slider_dist = Slider(bar_dist, 'Distance', 0, 10.0)
+
+def plot_data(feature):
+    all_data = get_all_data(feature)
+    rreturn_means = []
+    num = 75
+    for i in range(num):
+        rreturn_mean = []
+        for data in all_data:
+            if i < len(data):
+                rreturn_mean.append(data[i][1])
+        rreturn_means.append(rreturn_mean)
+    ax1.cla()
+    ax1.set_title('Dynamic Plot')
+    ax1.set_xlabel('Number of frames X 2048')
+    ax1.set_ylabel('Mean rreturn_mean')
+    ax1.boxplot(rreturn_means)
+    print(feature)
+
+
+def update(val):
+    # amp = samp.val
+    # freq = sfreq.val
+    # l.set_ydata(amp*np.sin(2*np.pi*freq*t))
+    # fig.canvas.draw_idle()
+    # print(val)
+    dist = closest(list(dist_feature_map.keys()), val)
+    feature = dist_feature_map[dist]
+    # update canvas
+    plot_data(feature)
+
+slider_dist.on_changed(update)
+
 plt.show()
