612a0dba0b163fdadd93ac45371bd18d3597f229
Ruoxi Li <rli12314@login-3.deepthought2.umd.edu>
2020-11-11 22:43:36 -0500

    exp3 more data

--- gen_bash_scripts_exp3.py
+++ gen_bash_scripts_exp3.py
@@ -1,4 +1,4 @@
-for size in range(2,17):
+for size in range(16,33):
     with open('{}_{}_{}_exp3.sh'.format(1,0,size), 'w+') as f:
         f.write('#!/bin/sh\n')
         f.write('python3 -m scripts.train.py --algo ppo --save-interval 10 --lr 0.001 --procs 8 --spec {} {} {}'.format(1,0,size))
--- test_all_exp3.bash
+++ test_all_exp3.bash
@@ -1,7 +1,7 @@
 #!/bin/sh
-for i in {1..19}
+for i in {1..20}
 do
-for s in {2..16}
+for s in {16..32}
 do
   sbatch -n 1 -N 1 --share -t 0:15:00 ./1_0_${s}_exp3.sh
 done
